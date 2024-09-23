0f17f0d6e9e9a3bb563c9716e13e6a60a7132a77
jose jimenez <jimenjos000@warren.k12.in.us>
2024-09-23 07:51:35 -0400

    bvbm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Main {
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12, 23));
+        System.out.println(hasSharedDigit(9, 99));
+        System.out.println(hasSharedDigit(12, 34));
+    }
+
+    private static boolean hasSharedDigit(int first, int second) {
+        if(first < 10 || first > 99){
+            return false;
+        }
+        if(second < 10 || second > 99){
+            return false;
+        }
+
+        int secondCopy = second;
+
+        while (first > 0){
+            int firstCheck = first % 10;
+            //Get the last digit of first with mod store in firstCheck
+            while(second > 0){
+                int secondCheck = second % 10;
+                //Get the last digit of second with mod store in secCheck
+                if(firstCheck == secondCheck){
+                    return true;
+                }
+                second /= 10;
+            }
+            first /= 10;
+            second = secondCopy;
+        }
+
+        return false;
+
+    }
 }
